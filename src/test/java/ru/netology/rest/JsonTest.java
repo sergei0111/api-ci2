cd9161d0f258930d99b7f088a443f3111387ed28
sergei <osp11011983@mail.ru>
2024-04-12 11:14:18 +0200

    add test

--- src/test/java/ru/netology/rest/JsonTest.java
+++ src/test/java/ru/netology/rest/JsonTest.java
@@ -1,8 +1,7 @@
-package ru.netology.rest;
-
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class JsonTest {
     @Test
@@ -17,6 +16,7 @@ public class JsonTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200);
-        //.body(/* --> ваша проверка здесь <-- */);
-    }}
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
